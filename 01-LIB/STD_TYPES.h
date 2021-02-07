6e61782fcffe1749a11bbd6c6f483ab6f9952b7a
Sahar Hassan <saharhassan.fci@gmail.com>
2021-02-07 19:54:51 +0200

    Library files is used in all layers

---
+++ 01-LIB/STD_TYPES.h
@@ -0,0 +1,20 @@
+/**********************************************************************/
+/** Author : Sahar Hassan Nooh ****************************************/
+/** Date   : 2 feb  2021       ****************************************/
+/** Version:v01                ****************************************/
+/**********************************************************************/
+
+#ifndef STD_TYPES_H_
+#define STD_TYPES_H_
+/*			1 BYTES							*/
+typedef		unsigned char					u8;
+/*			2 BYTES							*/
+typedef		unsigned short int				u16;
+/*			4 BYTES							*/
+typedef		unsigned int					u32;
+
+typedef		signed char						s8;
+typedef		signed short int				s16;
+typedef		signed int						s32;
+
+#endif /* STD_TYPES_H_ */
